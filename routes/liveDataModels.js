24c3206eeedd6d9ffb130e9958c2936a22a8b4fd
masonkarsevar <masonkarsevar@gmail.com>
2020-01-15 13:27:20 -0800

    set limit to queries

--- routes/liveDataModels.js
+++ routes/liveDataModels.js
@@ -15,5 +15,6 @@ function findRecordOneMonthAgo(monthAgo, exchange, trading_pair) {
   return dataScienceDb("candlesticks")
     .where({ exchange })
     .where({ trading_pair })
-    .where("timestamp", ">", monthAgo);
+    .where("timestamp", ">", monthAgo)
+    .limit(100);
 }
