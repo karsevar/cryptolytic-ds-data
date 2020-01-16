3844af7a2307465146cc665b94e6d0619cd7ad0d
masonkarsevar <masonkarsevar@gmail.com>
2020-01-16 13:10:11 -0800

    livedataroute reestablished

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-// server.use("/liveDataRoute", liveDataRoute);
+server.use("/liveDataRoute", liveDataRoute);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "hello world from base server" });
