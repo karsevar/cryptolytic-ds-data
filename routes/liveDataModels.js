5bb9f16f745aab000e28cb7c7e61183317c12543
masonkarsevar <masonkarsevar@gmail.com>
2020-01-17 15:14:55 -0800

    period set to 300

--- routes/liveDataModels.js
+++ routes/liveDataModels.js
@@ -2,18 +2,27 @@ const dataScienceDb = require("../data/dbConfig.js");
 
 module.exports = {
   findRecordByExchange,
-  findRecordOneMonthAgo
+  findRecordOneMonthAgo,
+  findRecordByExchangeAndPeriod
 };
 
 function findRecordByExchange(filter) {
   return dataScienceDb("candlesticks")
     .where(filter)
-    .limit(10);
+    .limit(1000);
 }
 
-function findRecordOneMonthAgo(monthAgo, exchange, trading_pair) {
+function findRecordOneMonthAgo(monthAgo, exchange, trading_pair, period) {
   return dataScienceDb("candlesticks")
     .where({ exchange })
     .where({ trading_pair })
+    .where({ period })
     .where("timestamp", ">", monthAgo);
+}
+
+function findRecordByExchangeAndPeriod(exchange, period) {
+  return dataScienceDb("candlesticks")
+    .where({ exchange })
+    .where({ period })
+    .limit(200);
 }
--- routes/liveDataRoute.js
+++ routes/liveDataRoute.js
@@ -1,10 +1,58 @@
 const express = require("express");
 const {
   findRecordByExchange,
-  findRecordOneMonthAgo
+  findRecordOneMonthAgo,
+  findRecordByExchangeAndPeriod
 } = require("./liveDataModels.js");
 
 const router = express.Router();
+
+router.post("/CompareTwoExchanges", (req, res) => {
+  var d = new Date();
+  d.setMonth(d.getMonth() - 2);
+
+  d.setHours(0, 0, 0);
+  d.setMilliseconds(0);
+
+  findRecordOneMonthAgo(
+    (d / 1000) | 0,
+    req.body.exchange_1,
+    req.body.trading_pair,
+    300
+  )
+    .then(result1 => {
+      findRecordOneMonthAgo(
+        (d / 1000) | 0,
+        req.body.exchange_2,
+        req.body.trading_pair,
+        300
+      )
+        .then(result2 => {
+          res
+            .status(200)
+            .json({ data: { exchange_1: result1, exchange_2: result2 } });
+        })
+        .catch(err => {
+          res.status(500).json({ message: err });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
+});
+
+router.post("/getExchangeDataByPeriod", (req, res) => {
+  findRecordByExchangeAndPeriod({
+    exchange: req.body.exchange,
+    period: req.body.period
+  })
+    .then(result => {
+      res.status(200).json({ data: result });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "command didnt work", err: error });
+    });
+});
 
 router.post("/getExchangeData", (req, res) => {
   findRecordByExchange({ exchange: req.body.exchange })
@@ -18,7 +66,7 @@ router.post("/getExchangeData", (req, res) => {
 
 router.post("/getDataByMonth", (req, res) => {
   var d = new Date();
-  d.setMonth(d.getMonth() - 1);
+  d.setMonth(d.getMonth() - 2);
 
   d.setHours(0, 0, 0);
   d.setMilliseconds(0);
@@ -26,7 +74,8 @@ router.post("/getDataByMonth", (req, res) => {
   findRecordOneMonthAgo(
     (d / 1000) | 0,
     req.body.exchange,
-    req.body.trading_pair
+    req.body.trading_pair,
+    300
   )
     .then(result => {
       res.status(200).json({ data: result, lastMonth: (d / 1000) | 0 });
@@ -44,7 +93,8 @@ router.post("/getDataByWeek", (req, res) => {
   findRecordOneMonthAgo(
     (d / 1000) | 0,
     req.body.exchange,
-    req.body.trading_pair
+    req.body.trading_pair,
+    300
   )
     .then(result => {
       res.status(200).json({ data: result, lastWeek: (d / 1000) | 0 });
@@ -56,13 +106,14 @@ router.post("/getDataByWeek", (req, res) => {
 
 router.post("/getDataByDay", (req, res) => {
   var d = new Date();
-  d.setHours(d.getHours() - 24);
+  d.setHours(d.getHours() - 36);
   d.setMilliseconds(0);
 
   findRecordOneMonthAgo(
     (d / 1000) | 0,
     req.body.exchange,
-    req.body.trading_pair
+    req.body.trading_pair,
+    300
   )
     .then(result => {
       res.status(200).json({ data: result, "24_hours_ago": (d / 1000) | 0 });
